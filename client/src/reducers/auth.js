ce596f5762df1558e72d9200a71f66284723209d
Will Koste <willkoste@gmail.com>
2020-08-15 23:05:55 -0400

    Sync For Home

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -22,6 +22,7 @@ export default function(state = initialState, action){
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.data);
       console.log(payload);
+      console.log(payload.user);
       return {
         ...state,
         ...payload,
